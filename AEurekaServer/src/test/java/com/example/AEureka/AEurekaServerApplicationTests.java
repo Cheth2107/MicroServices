e8d9801548c93acfbbf2e4f1a6e24f7cb1e07ce6
Chethan K <chethank0307@gmail.com>
2025-09-17 01:45:16 -0700

    services

---
+++ AEurekaServer/src/test/java/com/example/AEureka/AEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AEureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
